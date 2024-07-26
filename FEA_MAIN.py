7a547ff111cc106fbf9ef9c20a769cd924b88217
markday275 <125720467+markday275@users.noreply.github.com>
2024-07-26 17:52:20 +1200

    Add files via upload

---
+++ FEA_MAIN.py
@@ -0,0 +1,174 @@
+import numpy as np
+np.set_printoptions(linewidth=200)
+
+class Node:
+    """
+    Nodes class smallest thing in a FEA model\n
+    All other classes the built up by nodes. A node must have a name 
+    """
+    def __init__(self, name, XPos, YPos, DoFX, DoFY, EternalLoadX, EternalLoadY, ZPos = None, DoFZ = None, EternalLoadZ = None):
+        """
+        Class for Nodes that make up elements
+        """
+        self.name = name
+        self.XPos = XPos
+        self.YPos = YPos
+        self.ZPos = ZPos
+        self.DofX = DoFX
+        self.DofY = DoFY
+        self.DofZ = DoFZ
+        self.EternalLoadX = EternalLoadX
+        self.EternalLoadY = EternalLoadY
+        self.EternalLoadZ = EternalLoadZ
+        self.LocalDeflection = None
+        self.GlobalDeflection = None
+
+    def __repr__(self):
+        return (f"Node(name={self.name},"
+                f"LocalDeflection={self.LocalDeflection}, GlobalDeflection={self.GlobalDeflection})")
+        
+
+    def setLocalDeflection(self, deflectionlocal):
+        self.LocalDeflection = deflectionlocal
+
+    def setGlobalDeflection(self, deflectionGlobal):
+        self.GlobalDeflection = deflectionGlobal
+
+
+class BarElement:
+    """
+    Bar Element: Made up of 2 nodes\n
+    Alpha is assumed to be in degrees not needs if both element pos are defined
+    """
+    def __init__(self, name, alpha, E, A, L, node1: Node, node2: Node):
+        """
+        Class for Bar elements.
+        alpha is in degrees
+        """
+        self.name = name
+        self.alpha = alpha * np.pi / 180
+        self.E = E
+        self.A = A
+        self.L = L
+        self.node1 = node1
+        self.node2 = node2
+        self.localStiffnessmatrix = None
+        self.StiffnessMatrix = None
+        self.AssemblyMatrix = None
+
+    def __repr__(self):
+        return (f"BarElement(name={self.name}, "
+                f"node1={self.node1}, node2={self.node2}, "
+                f"localStiffnessmatrix=\n{self.localStiffnessmatrix}, "
+                f"StiffnessMatrix=\n{self.StiffnessMatrix})")
+    def setlocalStiffnessMatrix(self):
+        """
+        returns the global stiffness matrix as a numpy nested list ie [[],[]]
+        """
+        K_e = (self.E * self.A / self.L) * np.array([[1, -1],
+                                                     [-1, 1]])
+        self.localStiffnessmatrix = K_e
+
+
+        
+    def setStiffnessMatrix(self):
+        """
+        returns the stiffness matrix as a numpy nested list ie [[],[]]\n
+        IN GLOBAL CORDANATES 
+        """
+        if self.alpha is None:
+            alpha = np.arctan2(self.node2.YPos - self.node1.YPos, self.node2.XPos - self.node1.XPos)
+        else:
+            alpha = self.alpha
+
+        trans_matrix = np.array([[np.cos(alpha), np.sin(alpha), 0, 0],
+                                 [0, 0, np.cos(alpha), np.sin(alpha)]])
+        
+        if self.localStiffnessmatrix is None:
+            self.setlocalStiffnessMatrix()
+        K_e_global = np.transpose(trans_matrix) @ self.localStiffnessmatrix @ trans_matrix
+        self.StiffnessMatrix = K_e_global
+
+    def setAssemblyMatrix(self):
+        dofnum = int(self.node1.DofX) + int(self.node1.DofY) + int(self.node2.DofX) + int(self.node2.DofY) 
+        A_e = np.zeros((dofnum,4))
+        if self.node1.DofX :
+            A_e[0][0] = 1
+        if self.node2.DofX:
+            A_e[0][2] = 1
+        if self.node1.DofY:
+            A_e[1][1] = 1
+        if self.node2.DofY:
+            A_e[1][3] = 1
+        self.AssemblyMatrix = A_e
+
+
+class Structure:
+    """
+    Is the overall structure that is made up of elements.\n
+    Takes elements by add_elements
+    """
+    def __init__(self, name):
+        self.name = name
+        self.nodes = []
+        self.elements = []
+        self.GlobalStiffnessMatrix = None
+
+    def __repr__(self):
+        return (f"Structure(name={self.name},\n nodes=[{', '.join([str(node.name) for node in self.nodes])}], "
+                f"\nelements=[{', '.join([str(element.name) for element in self.elements])}])")
+
+
+    def add_element(self, element):
+        self.elements.append(element)
+        self.elements.sort(key=lambda s: s.name)
+        self.addNodes()
+
+    def addNodes(self):
+        """
+        adds all the nodes into a list for the assmebly matrix
+        should be called by add_element
+        """
+        for element in self.elements:
+            if element.node1 not in self.nodes:
+                self.nodes.append(element.node1)
+            if element.node2 not in self.nodes:
+                self.nodes.append(element.node2)
+        self.nodes.sort(key=lambda s: s.name)
+
+    def setGlobalStiffnessMatrix(self):
+        k_g_e = []
+        for element in self.elements:
+            if element.AssemblyMatrix is None:
+                element.setAssemblyMatrix()
+            if element.StiffnessMatrix is None:
+                element.setStiffnessMatrix()
+            k_g_e.append(element.AssemblyMatrix @ element.StiffnessMatrix @ element.AssemblyMatrix.T)
+        self.GlobalStiffnessMatrix = sum(k_g_e)
+     
+
+def main():
+    node1 = Node("Node1", 0, 0, False, False, 0, 0)
+    node2 = Node("Node2", 1, 0, True, True, -20000, 0)
+    node3 = Node("Node3", 0, 0, False, False, 0, 0)
+
+    bar1 = BarElement("bar1",alpha= 0,E= 200e9, A=400e-6, L=1.1, node1= node1, node2= node2)
+    bar2 = BarElement("bar2",alpha= 55,E= 200e9, A=600e-6, L=0.8, node1= node2, node2= node3)
+
+
+    structure = Structure("struct")
+    structure.add_element(bar1)
+    structure.add_element(bar2)
+
+    structure.setGlobalStiffnessMatrix()
+
+    print(structure.GlobalStiffnessMatrix)
+    
+
+if __name__ == "__main__":
+    main()
+
+
+        
+
+
