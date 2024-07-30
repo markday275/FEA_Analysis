3ed3c26beb0830280d3d52bc6f13a25279003024
msd60 <msd60@uclive.ac.nz>
2024-07-30 13:05:35 +1200

    changed program

--- FEA_program.py
+++ FEA_program.py
@@ -1,16 +1,16 @@
 import numpy as np
-import FEA_MAIN as fea
+from FEA_MAIN import *
 
 def main():
-    node1 = fea.Node("Node1", 0, 0, False, False, 0, 0)
-    node2 = fea.Node("Node2", 1, 0, True, True, 0, -20000)
-    node3 = fea.Node("Node3", 0, 0, False, False, 0, 0)
+    node1 = Node("Node1", 0, 0, False, False, 0, 0)
+    node2 = Node("Node2", 0, 0, True, True, 0, -20000)
+    node3 = Node("Node3", 0, 0, False, False, 0, 0)
 
-    bar1 = fea.BarElement("bar1",alpha= 0,E= 200e9, A=400e-6, L=1.1, node1= node1, node2= node2)
-    bar2 = fea.BarElement("bar2",alpha= 55,E= 200e9, A=600e-6, L=0.8, node1= node2, node2= node3)
+    bar1 = BarElement("bar1",alpha= 0,E= 200e9, A=400e-6, L=1.1, node1= node1, node2= node2)
+    bar2 = BarElement("bar2",alpha= 55,E= 200e9, A=600e-6, L=0.8, node1= node2, node2= node3)
 
 
-    structure = fea.Structure("struct")
+    structure = Structure("struct")
     structure.add_element(bar1)
     structure.add_element(bar2)
 
